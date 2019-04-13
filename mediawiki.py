592cee39d59487af5b33fda50a6c780f74af6005
Teemu Ikonen <tpikonen@gmail.com>
2019-04-13 21:22:09 +0300

    Use 'total_slots' in mediawiki.check_capacity()

--- mediawiki.py
+++ mediawiki.py
@@ -1254,7 +1254,8 @@ def check_capacity(os, ps):
     if ps["state"] != 'Station on':
         cell = "{} / N/A".format(oc if oc else "-")
         return (style_maybe, cell, "")
-    pc = str(int(ps['bikesAvailable']) + int(ps['spacesAvailable']))
+    #pc = str(int(ps['bikesAvailable']) + int(ps['spacesAvailable']))
+    pc = ps["total_slots"]
     if oc:
         if oc == pc:
             return (style_ok, pc, "")
